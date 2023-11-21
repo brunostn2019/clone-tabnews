6e80324c559ed778b37bfbf72de73e711f71145d
Bruno Soares Ferreira <bruno.lico@gmail.com>
2023-11-21 22:08:15 +0000

    adicionando frase, proposito e publico alvo ao meu site

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Bora jogar peteca amanhã?</h1>;
+  return (
+    <h1>
+      Aprenda SQL é um site para ajudar os iniciantes em SQL a praticar de forma
+      prática e rápida os primeiros passos da linguagem SQL. Tudo em portugues,
+      e voce só precisa de um navegador e internet!
+    </h1>
+  );
 }
 
 export default Home;
